ce918c6150c01e00d243fc73b07051fc2939b5d6
Steve Anderson <steve@fuzzysteve.co.uk>
2013-06-13 00:24:50 +0100

    display functions

---
+++ getData.php
@@ -0,0 +1,25 @@
+<?
+
+$dbh = new PDO('mysql:host=localhost;dbname=eve', 'eve', 'eve');
+
+
+$sql="select * from  balance order by id desc";
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+echo '{"cols":[{"id":"","label":"Date","type":"datetime"},{"id":"","label":"Value","type":"number"}],"rows":[';
+
+$x=1;
+while ($row=$stmt->fetchObject())
+{
+if ($x!=1)
+{
+echo ",";
+}
+$x=2;
+$date=localtime(strtotime($row->pointtime));
+$jsonformatdate="Date( ".($date[5]+1900).", ".($date[4]+1).", ".($date[3]+1).", ".($date[2]).", 0 , 0)";
+echo '{"c":[{"v":"'.$jsonformatdate.'"},{"v":'.$row->balance/$row->shares."}]}";
+}
+echo "]}";
+
+?>
