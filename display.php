587d98374a70f539587080dad32f8160d7fe1ce8
Steve Anderson <steve@fuzzysteve.co.uk>
2013-06-18 21:13:39 +0100

    shareholders added. current stock value added

--- display.php
+++ display.php
@@ -32,5 +32,50 @@
   <body>
     <!--Div that will hold the pie chart-->
     <div id="chart_div"></div>
-  </body>
+
+<div id="shareholders">
+<table>
+
+<thead>
+<tr><th>Name</th><th>Shares</th><th>Time of last entry</th></tr>
+</thead><tbody>
+<?
+
+$dbh = new PDO('mysql:host=localhost;dbname=eve', 'eve', 'eve');
+# current shareholders only
+$sql="select name,eveid,shares,lastupdate from shareholders where lastupdate=(select max(lastupdate) from  evesupport.shareholders)";
+
+#all shareholders in the last 5 months, along with the time they were last a shareholder, with the shares they held at that time.
+#$sql="select name,eveid,shares,max(lastupdate) from shareholders where lastupdate>date_sub(now(),INTERVAL 5 MONTH) group by name,eveid,shares";
+
+
+$stmt = $dbh->prepare($sql);
+
+$stmt->execute();
+while ($row = $stmt->fetchObject()){
+echo "<tr><td>".$row->name."</td><td>".$row->shares."</td><td>".$row->lastupdate."</td></tr>";
+}
+
+
+?>
+</tbody>
+</table>
+</div>
+<div class="CurrentStockValue">
+Stock value at current time:
+<?
+
+$sql="select * from balance order by id desc limit 1";
+$stmt = $dbh->prepare($sql);
+
+$stmt->execute();
+$row = $stmt->fetchObject();
+echo $row->balance/$row->shares;
+
+
+
+?>
+
+</div>
+</body>
 </html>
